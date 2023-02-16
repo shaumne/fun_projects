42152ad278405c3fb57da2e7cdfc0f9def08bc7d
shaumne <emrecanncetinkaya@gmail.com>
2023-02-17 01:11:24 +0300

    edited currency_api

--- currency_api/get_currency.py
+++ currency_api/get_currency.py
@@ -2,10 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def get_currency(from_: str, to: str):
+def get_currency(from_: str, to: str, amount: int):
     from_ = from_.upper()
     to = to.upper()
-    url = f"https://www.x-rates.com/calculator/?from={from_}&to={to}&amount=1"
+    url = f"https://www.x-rates.com/calculator/?from={from_}&to={to}&amount={amount}"
 
     content = requests.get(url).text
     soup = BeautifulSoup(content, "html.parser")
